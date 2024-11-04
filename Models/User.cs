ec80ad11ef4f63faf06910ac55b94a19c65628e7
Taha234672 <2345672@cegepmontpetit.ca>
2024-11-04 07:12:05 -0500

    FCT_Add User model

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Text.Json.Serialization;
+using WebAPI.Models;
+
+
+
+namespace FlappyBird.Models
+{
+    public class User : IdentityUser
+    {
+        [JsonIgnore]
+        public virtual List<Scores>? Scores { get; set; } = null;
+    }
+}
